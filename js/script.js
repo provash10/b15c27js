bced010af7cab609c1faf64efd80877b5c43deb6
provash10 <provash20cb@gmail.com>
2024-10-16 18:46:41 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// let rahim = "bike";
+// let karim = "car";
+// let rafiq = "cycle";
+
+let rahim = "car";
+let karim = "cycle";
+let rafiq = "bike";
+
+if(rahim == "bike"){  //true-false
+    console.log("I will go to dinner with Rahim");
+}
+
+else if(karim == "car"){
+    console.log("I will go to cox's bazar");
+}
+
+else if(rafiq == "cycle"){
+    console.log("I will go to field with Rafiq");
+}
+
+else{
+    console.log("I will go to sleep");
+}
